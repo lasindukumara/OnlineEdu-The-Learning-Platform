08feb520af4ecaf81f069628bd812f08908e7f24
lasindukumara <lasindukumara2020@gmail.com>
2023-12-16 11:37:12 +0530

    create user activate function

--- Backend/routes/user.route.ts
+++ Backend/routes/user.route.ts
@@ -1,7 +1,8 @@
 import express, { Express } from "express";
-import {registrationUser} from '../controllers/user.controller';
+import {activateUser, registrationUser} from '../controllers/user.controller';
 const UserRouter = express.Router();
 
 UserRouter.post('/registration',registrationUser);
+UserRouter.post('/activate-user',activateUser);
 
 export default UserRouter;
